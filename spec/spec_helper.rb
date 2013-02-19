22d988c3a0b48d71f6cbf22c33591d258778758a
Jay Perry <jperry@brightcove.com>
2013-02-18 20:46:57 -0500

    Make project example pending until I configure vcr and webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,3 @@ require 'rspec/autorun'
 require 'webmock/rspec'
 
 require File.expand_path('../../lib/teamcity', __FILE__)
-
-WebMock.disable_net_connect!(:allow_localhost => true)
--- spec/teamcity/client/projects_spec.rb
+++ spec/teamcity/client/projects_spec.rb
@@ -2,9 +2,11 @@ require_relative '../../spec_helper'
 
 describe 'Projects' do
   it 'should fetch projects' do
-    TeamCity.configure do |config|
-      config.endpoint = 'http://localhost:8111/guestAuth/app/rest/7.0/'
+    pending 'need to plugin vcr and webmock' do
+      TeamCity.configure do |config|
+        config.endpoint = 'http://localhost:8111/guestAuth/app/rest/7.0/'
+      end
+      TeamCity.projects.should_not be_nil
     end
-    TeamCity.projects.should_not be_nil
   end
 end
