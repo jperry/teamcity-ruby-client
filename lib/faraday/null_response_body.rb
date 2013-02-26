c9636ae943a53bf25ddf4327d31d54533e10ce51
Jay Perry <jperry@brightcove.com>
2013-02-25 23:54:21 -0500

    Add some documentation around the faraday middleware

--- lib/faraday/null_response_body.rb
+++ lib/faraday/null_response_body.rb
@@ -1,7 +1,11 @@
-# @private
 module FaradayMiddleware
-  # @private
+  # This middleware handles any responses where the body
+  # is 'null' which is the case when asking teamcity for
+  # an empty collection.  When this is encountered we
+  # set the response to an empty json object
   class NullResponseBody < Faraday::Middleware
+    # Faraday Middleware
+    # @note - should only be called by Faraday
     def call(env)
       @app.call(env).on_complete do |response|
         if response[:body] == "null"
