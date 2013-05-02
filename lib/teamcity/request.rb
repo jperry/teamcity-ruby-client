2d27395581cd0a31889ff4a84a706c159033f8c5
Jay Perry <jperry@brightcove.com>
2013-05-02 12:12:04 -0400

    Add the options params back since I'll be using those instead of the build locator in another branch

--- lib/teamcity/request.rb
+++ lib/teamcity/request.rb
@@ -2,32 +2,38 @@ module TeamCity
   # Defines HTTP request methods
   module Request
     # Perform an HTTP GET request
-    def get(path, &block)
-      request(:get, path, &block)
+    def get(path, options={}, &block)
+      request(:get, path, options, &block)
     end
 
     # Perform an HTTP POST request
-    def post(path, &block)
-      request(:post, path, &block)
+    def post(path, options={}, &block)
+      request(:post, path, options, &block)
     end
 
     # Perform an HTTP PUT request
-    def put(path, &block)
-      request(:put, path, &block)
+    def put(path, options={}, &block)
+      request(:put, path, options, &block)
     end
 
     # Perform an HTTP DELETE request
-    def delete(path, &block)
-      request(:delete, path, &block)
+    def delete(path, options={}, &block)
+      request(:delete, path, options, &block)
     end
 
     private
 
     # Perform an HTTP request
-    def request(method, path, &block)
+    def request(method, path, options, &block)
       response = connection.send(method) do |request|
         block.call(request) if block_given?
-        request.url(path)
+        case method
+        when :get, :delete
+          request.url(path, options)
+        when :post, :put
+          request.path = path
+          request.body = options unless options.empty?
+        end
       end
       response.body
     end
