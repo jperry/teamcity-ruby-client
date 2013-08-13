9e4ab1bd04cb8e551ba1254639c93a1c0c388f8c
Jay Perry <jperry@brightcove.com>
2013-08-13 19:56:16 -0400

    Change the headers class to allow mixing of headers

--- lib/teamcity/headers.rb
+++ lib/teamcity/headers.rb
@@ -3,36 +3,38 @@ module TeamCity
 
     VALID_FORMATS = [ :json, :xml, :text ]
 
-    def self.generate_for(format)
-      raise ArgumentError unless VALID_FORMATS.include?(format)
-      self.const_get(format.to_s.upcase).new
+    def self.build(opts={})
+      accept_format = opts.fetch(:accept){ :json }
+      content_type_format = opts.fetch(:content_type){ :json }
+      raise ArgumentError, "Invalid format for :accept, valid formats: #{VALID_FORMATS}" unless VALID_FORMATS.include?(accept_format)
+      raise ArgumentError, "Invalid format for :content_type, valid format: #{VALID_FORMATS}" unless VALID_FORMATS.include?(content_type_format)
+      new do |headers|
+        headers.accept = self.const_get(accept_format.to_s.capitalize).accept
+        headers.content_type = self.const_get(content_type_format.to_s.capitalize).content_type
+      end
+    end
+
+    attr_accessor :accept, :content_type
+
+    def initialize
+      yield(self) if block_given?
     end
 
     private
 
-    class HeaderDefinitions
-      def to_s
-        name
-      end
-
-      def name
-        self.class.name.split('::').last.downcase
-      end
+    class Xml
+      def self.accept; "application/xml; charset=utf-8"; end
+      def self.content_type; "application/xml"; end
     end
 
-    class XML < HeaderDefinitions
-      def accept; 'application/xml'; end
-      def content_type; 'application/json'; end
+    class Json
+      def self.accept; "application/xml; charset=utf-8"; end
+      def self.content_type; "application/json"; end
     end
 
-    class JSON < HeaderDefinitions
-      def accept; 'application/json'; end
-      def content_type; 'application/json'; end
-    end
-
-    class TEXT < HeaderDefinitions
-      def accept; 'text/plain'; end
-      def content_type; 'text/plain'; end
+    class Text
+      def self.accept; 'text/plain'; end
+      def self.content_type; 'text/plain'; end
     end
   end
 end
--- spec/teamcity/headers_spec.rb
+++ spec/teamcity/headers_spec.rb
@@ -2,41 +2,35 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 describe TeamCity::Headers do
 
-  before(:each) do
-    @klass = TeamCity::Headers
+  def build_headers(*args)
+    TeamCity::Headers.build(*args)
   end
 
-  it "should not accept an invalid format" do
-    expect { @klass.generate_for(:invalid_format) }.to raise_exception(ArgumentError)
+  it "should default the accept header to json" do
+    build_headers.accept.should eq("application/xml; charset=utf-8")
   end
 
-  it "should provide the accept header for json" do
-    headers = @klass.generate_for(:json)
-    headers.accept.should eq("application/json")
+  it "should default the content_type header to json" do
+    build_headers.content_type.should eq("application/json")
   end
 
-  it "should provide the content_type header for json" do
-    headers = @klass.generate_for(:json)
-    headers.content_type.should eq("application/json")
+  it "should allow you to set the accept" do
+    build_headers(:accept => :text).accept.should eq("text/plain")
   end
 
-  it "should provide the accept header for xml" do
-    headers = @klass.generate_for(:xml)
-    headers.accept.should eq("application/xml")
+  it "should not set the content_type when setting accept" do
+    build_headers(:accept => :text).content_type.should eq("application/json")
   end
 
-  it "should provide the content_type header for xml" do
-    headers = @klass.generate_for(:xml)
-    headers.content_type.should eq("application/json")
+  it "should allow you to set the content type" do
+    build_headers(:content_type => :text).content_type.should eq("text/plain")
   end
 
-  it "should provide the accept header for text" do
-    headers = @klass.generate_for(:text)
-    headers.accept.should eq("text/plain")
+  it "should not allow an invalid accept format" do
+    expect { build_headers(:accept => :invalid) }.to raise_exception(ArgumentError)
   end
 
-  it "should provide the conten_type header for text" do
-    headers = @klass.generate_for(:text)
-    headers.content_type.should eq("text/plain")
+  it "should not allow an invalid content_type format" do
+    expect { build_headers(:content_type => :invalid) }.to raise_exception(ArgumentError)
   end
 end
