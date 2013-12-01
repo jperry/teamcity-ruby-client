0d25ffdedae7a49c964099248b29cc6d205b18e0
Jefferson Girao <jgirao@thoughtworks.com>
2013-12-01 14:33:36 -0500

    Fixing typo

--- spec/teamcity/element_builder_spec.rb
+++ spec/teamcity/element_builder_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../spec_helper', __FILE__)
 require_relative '../spec_helper'
 
 describe TeamCity::ElementBuilder do
-  it 'outputs xml following TeamCity convention for elements and properties' do
+  it 'outputs json following TeamCity convention for elements and properties' do
     builder = TeamCity::ElementBuilder.new(:attr1 => 'x') do |properties|
       properties['property1'] = 'z'
     end
@@ -20,7 +20,7 @@ describe TeamCity::ElementBuilder do
     }.to_json
   end
 
-  it 'outputs xml following TeamCity convention for elements even if no properties are defined' do
+  it 'outputs json following TeamCity convention for elements even if no properties are defined' do
     builder = TeamCity::ElementBuilder.new(:attr1 => 'x')
 
     builder.to_request_body.should == {
